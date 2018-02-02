384bd0c13ce8dd9a1739ff5b326ed3f2f295e2eb
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-02-02 06:34:27 +0000

    Added all the subtemplates and standalones in bobregistration.py(methods)

--- bobregistration.py
+++ bobregistration.py
@@ -15,7 +15,7 @@ def plone_addon():
     reg.plonecli_alias = 'addon'
     return reg
 
-#buildout
+#standalone
 def plone_buildout():
     reg = RegEntry()
     reg.template = 'bobtemplates.plone:buildout'
@@ -27,5 +27,28 @@ def plone_theme():
     reg = RegEntry()
     reg.template = 'bobtemplates.plone:theme'
     reg.plonecli_alias = 'theme'
+    reg.depend_on = 'plone_addon'
+    return reg
+
+#standalone
+def plone_theme_package():
+    reg = RegEntry()
+    reg.template = 'bobtemplates.plone:theme_package'
+    reg.plonecli_alias = 'plone_theme_package'
+    return reg
+
+#sub template
+def plone_content_type():
+    reg = RegEntry()
+    reg.template = 'bobtemplates.plone:content_type'
+    reg.plonecli_alias = 'plone_content_type'
+    reg.depend_on = 'plone_addon' 
+    return reg
+
+#sub template
+def plone_vocabulary():
+    reg = RegEntry()
+    reg.template = 'bobtemplates.plone:vocabulary'
+    reg.plonecli_alias = 'plone_vocabulary'
     reg.depend_on = 'plone_addon'
     return reg
